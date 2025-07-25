e838e54e94aa450b89cd02cce5ec3e49c7a019c5
Acoidan Francisco Negrín Socorro <acoidanns@gmail.com>
2025-07-25 17:19:22 +0100

    Hide debug banner in MaterialApp
    Set debugShowCheckedModeBanner to false to remove the debug banner from the app during development.

--- dona_vida/lib/main.dart
+++ dona_vida/lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dona Vida',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
